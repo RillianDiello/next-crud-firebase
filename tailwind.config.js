5d9366f99cbc9d76496a0589f9b0ae6007dca1ce
RillianDiello <rilliansf@gmail.com>
2022-02-18 22:55:54 -0400

    Settings Files

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  purge: {
+    content: [
+      './src/pages/**/*.{js,ts,jsx,tsx}',
+      './src/components/**/*.{js,ts,jsx,tsx}'
+    ],
+    
+    safelist: [
+      'from-green-400',
+      'from-blue-400',
+      'from-gray-400',
+      'from-red-400',
+      'to-gray-700',
+      'to-blue-700',
+      'to-green-700', 
+      'to-red-700',     
+    ],
+  },
+  darkMode: 'class', // false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
